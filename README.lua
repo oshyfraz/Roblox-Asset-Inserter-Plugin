01c864178f20d6bad2034bcf7dfae920755e48b0
oshyfraz <159646580+oshyfraz@users.noreply.github.com>
2025-07-27 14:59:20 -0400

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,4 @@
+--[[ Roblox-Asset-Inserter-Plugin
+I can't post this on roblox due to the fact I am using the :LoadAsset() function, so here is the source code!
+To make this plugin, start by inserting a script, then paste the source code within this repository, and then lastly, export the script as a plugin.
+]]
