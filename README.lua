f677a6b2c9381a7b4e7ae1d9d57acc3d1ee7c457
oshyfraz <159646580+oshyfraz@users.noreply.github.com>
2025-07-27 15:05:01 -0400

    Update README.lua

--- README.lua
+++ README.lua
@@ -3,7 +3,7 @@ I can't post this on roblox due to the fact I am using the :LoadAsset() function
 
 This plugin allows you to insert any roblox asset via its asset id! I made this so you can insert items made by roblox or another PUBLIC developer that you can't directly reach in the toolbox, but know the link to.
 
-To make this plugin, start by inserting a script, then paste the source code within this repository, and then lastly, export the script as a plugin.
+To make this plugin, start by inserting a script (Preferably in ServerScriptService), then paste the source code within this repository into said script, and then lastly, export the script as a plugin.
 
 To use this plugin, you input an asset ID (numbers only) into the text box.  When you press enter, the asset, if found, will appear as a child of whatever you have selected in the explorer tab
 ]]
